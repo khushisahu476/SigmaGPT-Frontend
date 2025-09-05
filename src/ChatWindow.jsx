e7af3a4ed0c9a19064350c46051d2603203fc2d2
khushisahu476 <146587867+khushisahu476@users.noreply.github.com>
2025-09-06 03:08:51 +0530

    new change url

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -27,7 +27,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8080/api/chat", options);
+            const response = await fetch("http://13.51.196.121:8080/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
